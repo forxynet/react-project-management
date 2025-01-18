4d470fe1f43b4cfe235512a9b7f55a514ec56395
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-19 02:00:52 +0300

    added filter logic to dashboard

---
+++ src/components/CommentList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Avatar from './Avatar'
+
+export default function CommentList({ project }) {
+  return (
+    <>
+      {project.comments.length > 0 && <>
+        <h4>Project Comments</h4>
+        <ul>
+          {project.comments.map(comment => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                <p>date here</p>
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </>
+      }
+    </>
+  )
+}
