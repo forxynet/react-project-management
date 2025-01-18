6ce6c57228f1378c14155aade9f8c04c42b18fce
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-19 02:34:31 +0300

    complated app

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/storage'
 
-// you should put your configuration below
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyB8Bo8dP-17ser5he63bHLS6dFRanXTU6c",
+  authDomain: "thedojosite-b9d77.firebaseapp.com",
+  projectId: "thedojosite-b9d77",
+  storageBucket: "thedojosite-b9d77.firebasestorage.app",
+  messagingSenderId: "27232498956",
+  appId: "1:27232498956:web:61a3cfdada36bd09c20529"
 };
 
 
