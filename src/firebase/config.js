3209d2babb680244c7fd108becb1d1e1689a1aaa
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-21 18:17:24 +0300

    updated filter of dashboard

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,21 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 // put your config below
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: ""
+// };
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBn_Dsj85etLz_8sjKr1qLQCbwrSPeOmFI",
+  authDomain: "thedojosite-96eec.firebaseapp.com",
+  projectId: "thedojosite-96eec",
+  storageBucket: "thedojosite-96eec.firebasestorage.app",
+  messagingSenderId: "295968290407",
+  appId: "1:295968290407:web:a575f9fc75c8318ceec6c2"
 };
 
 // init firebase
--- src/pages/dashboard/ProjectFilter.js
+++ src/pages/dashboard/ProjectFilter.js
@@ -1,21 +1,24 @@
-export default function ProjectFilter({ currentFilter, changeFilter }) {
-  const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+import { useState } from 'react'
 
-  function handleClick(newFilter) {
+const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+
+export default function ProjectFilter({ changeFilter }) {
+  const [currentFilter, setCurrentFilter] = useState('all')
+
+  const handleClick = (newFilter) => {
+    setCurrentFilter(newFilter)
     changeFilter(newFilter)
   }
 
   return (
-    <div className='project-filter'>
+    <div className="project-filter">
       <nav>
-        <p>Filter by:</p>
+        <p>Filter by: </p>
         {filterList.map((f) => (
           <button key={f}
             onClick={() => handleClick(f)}
             className={currentFilter === f ? 'active' : ''}
-          >
-            {f}
-          </button>
+          >{f}</button>
         ))}
       </nav>
     </div>
